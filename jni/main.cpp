ace51fdb132ec092b6b94b30fd07228c357afeec
Alexey Korepanov <alexkorep@gmail.com>
2012-09-16 18:44:01 +0400

    - Removed trash files - Started adding comments to the main.cpp file.
    Signed-off-by: Alexey Korepanov <alexkorep@gmail.com>

--- jni/main.cpp
+++ jni/main.cpp
@@ -1,34 +1,34 @@
 /*
- * Copyright (C) 2010 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+  AndFluids, Android port of FLUIDS - SPH Fluid Simulator
+  Copyright (C) 2012 Alexey Korepanov
+
+  ZLib license
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely, subject to the following restrictions:
+
+  1. The origin of this software must not be misrepresented; you must not
+     claim that you wrote the original software. If you use this software
+     in a product, an acknowledgment in the product documentation would be
+     appreciated but is not required.
+  2. Altered source versions must be plainly marked as such, and must not be
+     misrepresented as being the original software.
+  3. This notice may not be removed or altered from any source distribution.
+
  */
 
 #include <android_native_app_glue.h>
 #include <android/native_window.h>
 #include <android/input.h>
 #include <android/looper.h>
-
-//#include <errno.h>
-//#include <jni.h>
-//#include <sys/time.h>
-//#include <time.h>
 #include <android/log.h>
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <math.h>
 
 #include "fluids/fluids/fluid_system.h"
 
@@ -37,6 +37,13 @@
 #define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
+/**
+ * @class MyFluidSystem
+ *
+ * This class extends FluidSystem from the FLUIDS library to extend drawing
+ * functions in order to output particles to the Android native screen buffer.
+ *
+ */
 class MyFluidSystem: public FluidSystem {
 private:
 	int m_xMin;
